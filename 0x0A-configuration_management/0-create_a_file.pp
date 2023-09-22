b022e18dac737c77e6e72e78de0fce6e589b7d3b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-22 19:54:42 +0200

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,10 @@
 # Creates file
 
 file { 'school':
-      
+  ensure   => file,
+  path     => '/tmp/school',
+  conttent => 'I love Puppet',
+  owner    => 'www-data',
+  group    => 'www-data',
+  mode     => '0744',
+}
