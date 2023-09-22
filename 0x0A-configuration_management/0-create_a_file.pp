5f591f6fea0159c0d74f17e5a63de67e00df175e
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-23 00:59:28 +0200

    two  files fixes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Creates file
 
 file { '/tmp/school':
-  ensure   => file,
-  path     => '/tmp/school',
+  ensure   => 'file',
   conttent => 'I love Puppet',
   owner    => 'www-data',
   group    => 'www-data',
