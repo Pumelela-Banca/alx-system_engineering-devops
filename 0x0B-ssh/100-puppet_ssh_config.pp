4f32ce842a6472b299da26a72326372b13a4cb16
Pumelela-Banca <pumelelabanca@gmail.com>
2023-09-25 02:06:20 +0200

    puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,10 @@
-#Uses  pupppet
+#!/usr/bin/env bash
+# Uses  pupppet
+
+file { 'ect/ssh/ssh_config':
+  ensure  => present;
+  content => "
+  host*
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no"
+}
