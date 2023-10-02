1dcaee1357fe925d73ec2e7ccca241c8d0cfffdb
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-02 23:14:10 +0200

    added pupped file will all lines added

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,6 +17,30 @@ file_line { 'write content':
   after    => 'server_name _;',
 }
 
+file { '/var/www/html/index.nginx-debian.html':
+ ensure    => present,
+ content   => "Hello world!",
+}
+
+file { '/var/www/html/404.html':
+ ensure    =>present,
+ content   => "Ceci n'est pas une page",
+}
+
+file_line { 'write redirect':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  line     => "rewrite ^\/redirect_me https:\/\/github.com\/Pumelela-Banca permanent;",
+  after    => 'server_name _;',
+}
+
+file_line { 'write redirect 404':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  line     => "\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t}",
+  after    => 'listen 80 default_server;',
+}
+
 servirce { 'nginx':
   ensure => running,
 }
