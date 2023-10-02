c080a120cbcf21a9fdaad7b91d9c1e7cf16fdf0c
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-02 21:30:56 +0200

    uses puppet to configure second server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,26 @@
 #!/usr/bin/env bash
+
+exec { 'update':
+  command  => 'sudo apt-get update',
+  path     => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'install nginx':
+  command  => 'sudo apt-get install -y nginx',
+  path     => '/usr/bin:/usr/sbin:/bin',
+}
+
+file_line { 'write content':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  line     => "\tadd_header X-Served-By ${hostname};",
+  after    => 'server_name _;',
+}
+
+file { '/var/www/html/index.html':
+ content   =>'Holberton School',
+}
+
+servirce { 'nginx':
+  ensure => running,
+}
