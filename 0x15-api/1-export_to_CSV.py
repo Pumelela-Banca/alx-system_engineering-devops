3ad0e9a1f0e6672feea9cd78d5bb25fc78d71c5d
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-24 01:23:33 +0200

    saves data to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Returns information about employee TO-DO list and progress.
+Also exports to cvv
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    test_url = "https://jsonplaceholder.typicode.com"
+    usr_name = requests.get(
+        test_url + f"/users/{sys.argv[1]}").json()
+    tasks_td = requests.get(
+        test_url + f"/users/{sys.argv[1]}/todos").json()
+    tasks_complete = [task for task in tasks_td
+                      if task.get("completed")]
+
+    with open(f"{sys.argv[1]}.csv", 'w') as file:
+        for x in tasks_td:
+            line = f'"{usr_name.get("id")}",' \
+                   f'"{usr_name.get("name")}",' \
+                   f'"{x.get("completed")}",' \
+                   f'"{x.get("title")}"'
+            file.writelines(line + "\n")
