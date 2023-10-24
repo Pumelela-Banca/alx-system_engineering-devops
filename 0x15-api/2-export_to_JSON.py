f87736502cb3ed193210a6c52359f95ecfbe2e18
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-24 02:29:35 +0200

    only save user data

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
                   "TASK_TITLE"]
         all_vals = []
         for x in tasks_td:
+            if int(sys.argv[1]) != x.get("userID"):
+                continue
             user_id = sys.argv[1]
             user_name = usr_name.get("username")
             completed = x.get("completed")
