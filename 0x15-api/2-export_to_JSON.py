b3174155e2c8bf8d9e717864316b7cb39f55f44e
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-24 02:17:10 +0200

    saves values into json format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,8 +5,10 @@ Returns information about employee TO-DO list and progress.
 Also exports to cvv
 """
 
+import json
 import requests
 import sys
+
 
 if __name__ == "__main__":
 
@@ -17,3 +19,20 @@ if __name__ == "__main__":
         test_url + f"/users/{sys.argv[1]}/todos").json()
     tasks_complete = [task for task in tasks_td
                       if task.get("completed")]
+
+    with open(f"{sys.argv[1]}.json", 'w') as file:
+        titles = ["USER_ID",
+                  "USERNAME", "TASK_COMPLETED_STATUS",
+                  "TASK_TITLE"]
+        all_vals = []
+        for x in tasks_td:
+            user_id = sys.argv[1]
+            user_name = usr_name.get("username")
+            completed = x.get("completed")
+            title = x.get("title")
+            stor_dict = {titles[0]: user_id,
+                         titles[1]: user_name,
+                         titles[2]: completed,
+                         titles[3]: title}
+            all_vals.append(stor_dict)
+        json.dump({sys.argv[1]: all_vals}, file)
