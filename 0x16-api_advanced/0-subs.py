a4c7b1653078d9e9afa35a57cb85bf1adba8cb29
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-07 22:25:52 +0200

    reduced line length

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,8 @@ def number_of_subscribers(subreddit):
     gets subs from  reddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url,
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    response = requests.get(
+        url, headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get("subscribers")
     else:
