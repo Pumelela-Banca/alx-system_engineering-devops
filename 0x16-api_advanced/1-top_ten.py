8045bdbe20216ae4412e44a5079e02a095cae781
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-07 23:02:15 +0200

    gets top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,3 +4,19 @@
 unpacks files inside servers
 """
 
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints  subreddit
+    """
+    url = url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(
+        url, headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        for chld in data.get("children")[:10]:
+            print(chld.get("data").get("title"))
+    else:
+        print("None")
