01434d4d6193b34a51855af9eabb618779f86cac
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-17 04:06:04 +0200

    added sed

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # allow user holberton to access files
 
 exec { 'increase file limit':
-  command => '/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
 
 exec { 'increase file soft limit':
-  command => '/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i  "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
