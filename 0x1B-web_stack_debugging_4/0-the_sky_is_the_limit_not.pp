da0c3dfefcce53063e0e3778e07cdfa393335efc
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-17 04:07:40 +0200

    added slash after number

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increasing traffic
 
 exec { 'increase traffic':
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin',
 }
 
