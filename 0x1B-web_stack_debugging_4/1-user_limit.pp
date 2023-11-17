fc8225c559eed2414acfb2725ce90724da4db19d
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-17 04:04:12 +0200

    increased soft limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,3 +4,8 @@ exec { 'increase file limit':
   command => '/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
 }
+
+exec { 'increase file soft limit':
+  command => '/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
