06da1659389c3c5d1b566bf00f15ae5ae8908fb7
Pumelela-Banca <pumelelabanca@gmail.com>
2023-11-17 04:01:55 +0200

    increase limit on user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 # allow user holberton to access files
 
 exec { 'increase file limit':
-
+  command => '/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
